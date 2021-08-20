bb0515cc18424a9013a586c0258152b44b6191cf
hahriar <closebrocircle@gmail.com>
2021-08-20 12:14:41 +0600

    first commit

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,55 @@
-console.log('Have a Relax see you not for mind');
+document.getElementById('deposit-button').addEventListener('click', function(){
+   const depositInput = document.getElementById('deposit-input');
+   const depositAmountText = depositInput.value;
+   const depositAmount = parseFloat(depositAmountText);
+
+   //current deposit
+
+   const depositTotal = document.getElementById('deposit-total');
+   const depositTotalText = depositTotal.innerText;
+   const previousDepositTotal = parseFloat(depositTotalText);
+   depositTotal.innerText = previousDepositTotal + depositAmount;
+
+
+   // update Balance 
+
+   const balanceTotal = document.getElementById('balance-total');
+   const balanceTotalText = balanceTotal.innerText;
+   const previousbalanceTotal = parseFloat(balanceTotalText);
+   balanceTotal.innerText = previousbalanceTotal + depositAmount;
+
+
+
+   //reset input field
+
+   depositInput.value = '';
+});
+
+ // handle withdraw button 
+
+    document.getElementById('withdraw-button').addEventListener('click', function(){
+        const withdrawInput = document.getElementById('withdraw-input');
+        const withdrawAmountText = withdrawInput.value;
+        const withdrawAmount = parseFloat(withdrawAmountText);
+        
+
+        // update Withdraw Total 
+
+        const withdrawTotal = document.getElementById('withdraw-total');
+        const previousWithdrawTotalText = withdrawTotal.innerText;
+        const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+
+        withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+        // Balance Update after Withdraw 
+
+        const balanceTotal = document.getElementById('balance-total');
+        const balanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+
+        balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+        // reset withdraw input field 
+
+        withdrawInput.value = '';
+    });
